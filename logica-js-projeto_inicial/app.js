2be71b76a029cd329d5334014456b4257decbedd
Lucas Santiago <lucahh@gmail.com>
2024-01-18 13:15:31 -0300

    Inicio
    Aulas iniciais

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Boas Vindas ao jogo do número secreto');
+
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute){
+    console.log('Isso ai! Você descobriu o número secreto(5)');
+}
